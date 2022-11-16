6dfda1ee405b77cd1257bec8d38c2ff05f4bbc46
Nathan Menezes <112178629+nathanmenezes@users.noreply.github.com>
2022-11-16 03:31:49 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_kjq5xb3";
+    const templateID = "template_yjgdl4e";
+    emailjs.send(serviceID, templateID, params).then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Mensagem Enviada!")
+        })
+        .catch(err => console.log(err));
+}
